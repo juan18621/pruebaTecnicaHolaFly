a940c54c73005751b46119ee4ad3e7ff1dedf84a
Juan Rojas <andresjuan2001@gmail.com>
2023-09-11 15:05:58 -0500

    fix(logs) log bugs fixed

--- src/app/Log/routes/logRouter.js
+++ src/app/Log/routes/logRouter.js
@@ -3,9 +3,6 @@ const logController = require("../controllers/logController");
 
 const logRouter = require('express').Router()
 
-
 logRouter.get('/hfswapi/getLogs', logController.getLogs );
-
-
 
 module.exports = logRouter;
